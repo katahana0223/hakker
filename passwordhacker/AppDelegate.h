0f8f8f5ae75a4b32282820f6464a41ddb911ab2b
津川 <kataokahana@kataokayouna-no-MacBook-Air.local>
2014-12-17 19:40:29 +0900

    Initial Commit

---
+++ passwordhacker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  passwordhacker
+//
+//  Created by 津川 on 2014/12/17.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ passwordhacker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  passwordhacker
+//
+//  Created by 津川 on 2014/12/17.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
