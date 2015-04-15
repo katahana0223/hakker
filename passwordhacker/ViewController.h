40891b87fde0befe06ce885ece1a562559ba6c75
津川 <hanahanamocomoco@softbank.ne.jp>
2015-04-15 20:38:42 +0900

    firstcommit

--- passwordhacker/ViewController.h
+++ passwordhacker/ViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    IBOutlet UILabel *resultLabel1 ; //結果（一番右のラベル）
+    IBOutlet UILabel *resultLabel2 ; //結果（右から２番目のラベル）
+    IBOutlet UILabel *resultLabel3 ; //結果（右から３番目のラベル）
+    IBOutlet UILabel *resultLabel4 ; //結果（右から４番目のラベル）
+    IBOutlet UILabel *countlabel;   //カウントの中の数字を表すラベル
+    
+    int password;    //探し出す数字
+    int digit[4];
+}
+-(IBAction)start;    //ボタンを押したときの処理
 @end
--- passwordhacker/ViewController.m
+++ passwordhacker/ViewController.m
@@ -18,7 +18,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    resultLabel1.text= @"?" ;
+    resultLabel2.text= @"?" ;
+    resultLabel3.text= @"?" ;
+    resultLabel4.text= @"?" ;
+    countlabel.text= @"0" ;
+    password=9933;
 }
+-(IBAction)start{
+    for(int i = 0 ;i <10000 ; i = i + 1){
+        countlabel.text =[NSString stringWithFormat:@"%d",i]  ;
+        [[NSRunLoop currentRunLoop]
+        runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.0005]
+         ] ;
+        if(i ==password){
+            for (int j = 0 ; j < 4 ; j = j + 1){
+                digit [j]= (password % 10);
+                password=password/10;
+            }
+            resultLabel1.text =[NSString stringWithFormat:@"%d",digit[0]];
+            resultLabel2.text =[NSString stringWithFormat:@"%d",digit[1]];
+            resultLabel3.text =[NSString stringWithFormat:@"%d",digit[2]];
+            resultLabel4.text =[NSString stringWithFormat:@"%d",digit[3]];
+
+
+
+        }
+
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
